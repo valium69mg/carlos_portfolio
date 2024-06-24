b96638d05473d7d5a3fa8f423d96dfe915278e2f
valium69mg <carlostranquilino.cr@gmail.com>
2024-06-24 12:54:43 -0600

    Change photo border style and add automatic year to copyright year footer
    1. Change border of photo to be blue and of 3px
    2. Change name of the author on the footer to my full name
    3. Change hard coded year to obtaining current year through js

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,7 +2,7 @@ import "./styles/footer.css";
 function Footer() {
     return <footer className="footer">
     <p>Author: Carlos Roman </p>
-    <p>Copyright: 2024 </p>
+    <p>Copyright © { new Date().getFullYear()} </p>
     <p><a href="/"> Home page </a></p>
   </footer> 
 }
