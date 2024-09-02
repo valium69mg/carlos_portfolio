8ff95528ef43640492ff64b5734da2cca28b9a29
valium69mg <xdeus.v2@gmail.com>
2024-09-02 12:43:41 -0600

    Add an img wrapper to the technologies logos

--- src/App.js
+++ src/App.js
@@ -101,13 +101,15 @@ let windowLogo = <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" 
 <path d="M2.5 4a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1m2-.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0m1 .5a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1"/>
 <path d="M2 1a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2zm13 2v2H1V3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1M2 14a1 1 0 0 1-1-1V6h14v7a1 1 0 0 1-1 1z"/>
 </svg>;
+//{props.description !== "" ? <p> {props.description.substring(0,100)+'...'}</p> : <p> </p>}
 
 function SkillCard(props) {
   return <div className="skillCard">
-          {props.img !== "" ?  <img src={props.img} alt="skill"></img> :<img src={GenericAvatar} alt="skill"></img>}
           <h1>{props.title} </h1>
-          {props.description !== "" ? <p> {props.description.substring(0,100)+'...'}</p> : <p> </p>}
-  </div>
+          <div class="imgWrapper">
+          {props.img !== "" ?  <img src={props.img} alt="skill"></img> :<img src={GenericAvatar} alt="skill"></img>}
+          </div>
+      </div>
 }
 
 
