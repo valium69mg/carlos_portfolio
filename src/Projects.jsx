c0fc8a3d543b5477cac4200b5540362136926ba9
valium69mg <xdeus.v2@gmail.com>
2024-09-02 14:42:17 -0600

    Change overral colour pallete

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -40,7 +40,7 @@ let backLogo = <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fi
 </svg>
 
 
-let youtubeLogo = <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="blue" className="bi bi-youtube" viewBox="0 0 16 16">
+let youtubeLogo = <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#6482AD" className="bi bi-youtube" viewBox="0 0 16 16">
 <path d="M8.051 1.999h.089c.822.003 4.987.033 6.11.335a2.01 2.01 0 0 1 1.415 1.42c.101.38.172.883.22 1.402l.01.104.022.26.008.104c.065.914.073 1.77.074 1.957v.075c-.001.194-.01 1.108-.082 2.06l-.008.105-.009.104c-.05.572-.124 1.14-.235 1.558a2.01 2.01 0 0 1-1.415 1.42c-1.16.312-5.569.334-6.18.335h-.142c-.309 0-1.587-.006-2.927-.052l-.17-.006-.087-.004-.171-.007-.171-.007c-1.11-.049-2.167-.128-2.654-.26a2.01 2.01 0 0 1-1.415-1.419c-.111-.417-.185-.986-.235-1.558L.09 9.82l-.008-.104A31 31 0 0 1 0 7.68v-.123c.002-.215.01-.958.064-1.778l.007-.103.003-.052.008-.104.022-.26.01-.104c.048-.519.119-1.023.22-1.402a2.01 2.01 0 0 1 1.415-1.42c.487-.13 1.544-.21 2.654-.26l.17-.007.172-.006.086-.003.171-.007A100 100 0 0 1 7.858 2zM6.4 5.209v4.818l4.157-2.408z"/>
 </svg>
 
@@ -49,11 +49,11 @@ let exitLogo = <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fi
 <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
 </svg>
 
-let infoLogo = <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="blue" className="bi bi-info-circle-fill" viewBox="0 0 16 16">
+let infoLogo = <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#6482AD" className="bi bi-info-circle-fill" viewBox="0 0 16 16">
 <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2"/>
 </svg>
 
-let certificateLogo = <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="blue" className="bi bi-card-text" viewBox="0 0 16 16">
+let certificateLogo = <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-card-text" viewBox="0 0 16 16">
 <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2z"/>
 <path d="M3 5.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5M3 8a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 8m0 2.5a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5"/>
 </svg>
@@ -279,6 +279,7 @@ function Projects() {
         </>    
         {loading !== true ? 
         <>  
+            
             <div className="projectsContainer">
                 <ProjectCategory 
                     key="Fullstack"
@@ -288,19 +289,19 @@ function Projects() {
                     />
             </div>
             <div className="projectsContainer">
-                <ProjectCategory 
-                    key="Front End"
-                    category="Front End Projects" 
-                    projects={projects}
-                    logo={frontLogo}
-                    />
-            </div>
-            <div className="projectsContainer">
                 <CertificateCategory
                     key="Udemy"
                     category="Certificados (Udemy)" 
                     projects={certifications}
                     logo={certificateLogo}
+                    />
+            </div>
+            <div className="projectsContainer">
+                <ProjectCategory 
+                    key="Front End"
+                    category="Front End Projects" 
+                    projects={projects}
+                    logo={frontLogo}
                     />
             </div>
             <Footer/>
