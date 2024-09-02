9f1ce3656bda23fd383e41168e22782484aeb919
valium69mg <xdeus.v2@gmail.com>
2024-09-02 13:27:22 -0600

    Modify navbar logo

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -3,9 +3,9 @@ import "./styles/navbar.css";
 import { useEffect,useState } from "react";
 
 function Svg() {
-    return <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill="blue" className="logoNavbar bi bi-pc" viewBox="0 0 16 16">
-        <path d="M5 0a1 1 0 0 0-1 1v14a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V1a1 1 0 0 0-1-1zm.5 14a.5.5 0 1 1 0 1 .5.5 0 0 1 0-1m2 0a.5.5 0 1 1 0 1 .5.5 0 0 1 0-1M5 1.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5M5.5 3h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1 0-1"/>
-    </svg>
+    return <svg xmlns="http://www.w3.org/2000/svg" width="42" height="42" fill="blue" className="logoNavbar bi bi-code-slash" viewBox="0 0 16 16">
+    <path d="M10.478 1.647a.5.5 0 1 0-.956-.294l-4 13a.5.5 0 0 0 .956.294zM4.854 4.146a.5.5 0 0 1 0 .708L1.707 8l3.147 3.146a.5.5 0 0 1-.708.708l-3.5-3.5a.5.5 0 0 1 0-.708l3.5-3.5a.5.5 0 0 1 .708 0m6.292 0a.5.5 0 0 0 0 .708L14.293 8l-3.147 3.146a.5.5 0 0 0 .708.708l3.5-3.5a.5.5 0 0 0 0-.708l-3.5-3.5a.5.5 0 0 0-.708 0"/>
+  </svg>
 }
 
 function Navbar() {
