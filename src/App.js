250271d9908f9c1fbe78533f794c42cb5773c255
valium69mg <xdeus.v2@gmail.com>
2024-09-21 17:32:26 -0600

    Take button out of resentation card and put it below it, also change style accordingly

--- src/App.js
+++ src/App.js
@@ -119,7 +119,7 @@ let skillsFramework = [
   },
 ]
 
-let downloadLogo = <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" className="bi bi-box-arrow-down" viewBox="0 0 16 16">
+let downloadLogo = <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#16325B" className="bi bi-box-arrow-down" viewBox="0 0 16 16">
 <path fillRule="evenodd" d="M3.5 10a.5.5 0 0 1-.5-.5v-8a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 .5.5v8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 0 0 1h2A1.5 1.5 0 0 0 14 9.5v-8A1.5 1.5 0 0 0 12.5 0h-9A1.5 1.5 0 0 0 2 1.5v8A1.5 1.5 0 0 0 3.5 11h2a.5.5 0 0 0 0-1z"/>
 <path fillRule="evenodd" d="M7.646 15.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 14.293V5.5a.5.5 0 0 0-1 0v8.793l-2.146-2.147a.5.5 0 0 0-.708.708z"/>
 </svg>;
@@ -184,9 +184,10 @@ function App() {
             </img>
             </div>
             
-            <a className="descargarBtn" href={cv}  target="_blank" rel="noopener noreferrer"> Descargar c.v.  {downloadLogo} </a>
           
         </div>
+        <a className="descargarBtn" href={cv}  target="_blank" rel="noopener noreferrer"> Descargar c.v.  {downloadLogo} </a>
+
         <div className="projectsContainer">
                 <ProjectCategory 
                     key="Destacado"
